24c55539e3de66e6b85e7117c45f0cf12a4ef7eb
jaksa <jaksa76@gmail.com>
2025-08-31 16:41:16 +0000

    added benchmark script

---
+++ benchmark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROMPT_DIR="prompts"
+AGENT="./tdd.sh"
+pids=()
+
+for prompt_file in "$PROMPT_DIR"/*.txt; do
+  (
+    $AGENT "$prompt_file"
+  ) &
+  pids+=($!)
+done
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
+
+echo "Benchmark finished."
